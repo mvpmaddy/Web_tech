9e80d6d146571766740fa1ee6221a5bd185b929c
Madan Patil <mvp.maddy123@gmail.com>
2020-12-07 07:09:15 +0530

    Files added

---
+++ backend/data.js
@@ -0,0 +1,35 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default  {
+    products: [
+        {
+        _id:'1',
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image: '/images/s1.jpg',
+        price: 1000,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+        },
+        {
+        _id:'2',
+        name: 'Fit Shirt',
+        category: 'Shirts',
+        image: '/images/s2.jpg',
+        price: 800,
+        brand: 'Puma',
+        rating: 4.3,
+        numReviews: 11
+        },
+        {
+        _id:'3',
+        name: 'Regular Pant',
+        category: 'Pants',
+        image: '/images/p1.jpg',
+        price: 1100,
+        brand: 'Nike',
+        rating: 4.8,
+        numReviews: 13
+        }
+    ]
+}
